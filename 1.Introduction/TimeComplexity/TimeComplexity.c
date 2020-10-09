121a1fbc5282dd954ad614a634027c16f82cf064
crow <l0serh40615@gamil.com>
2020-10-09 12:01:17 +0800

    init repo

---
+++ 1.Introduction/TimeComplexity/TimeComplexity.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void analysis(int n)
+{
+  int i,j;
+  int count = 0;
+  int num = 1;
+  for (i = 0; i < n; i++)
+    for (j = i; j < n; j++) {
+          num = num * 2;
+          count++;
+    }
+  printf("Ö´ÐÐ´ÎÊý:%d",count);
+}
+
+int main() {
+  analysis(5);
+  return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/DataStructure_C137.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure_C137.dir/1.Introduction/TimeComplexity/TimeComplexity.c.obj"
+  "DataStructure_C137.exe"
+  "DataStructure_C137.exe.manifest"
+  "DataStructure_C137.pdb"
+  "libDataStructure_C137.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructure_C137.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
